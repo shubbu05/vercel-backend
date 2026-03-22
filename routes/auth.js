99ccd82bd726564372e66b3940762938014ac50b
shubbu05 <shubham.kansalji57@gmail.com>
2026-03-22 14:22:02 +0530

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,5 @@
 const express = require('express');
+console.log("✅ Auth routes file loaded");
 const router = express.Router();
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
